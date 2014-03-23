03797b90cf019f8657535eb219a69606a45be2da
renzhentaxi <renzhentaxibaerde@gmail.com>
2014-03-23 06:31:57 -0400

    Update and rename choices.java to function.java
    How to use:
    String[] a = {"lol","haha"} <- options;
    Int Result;
    Result = function.show_message(string message, string[] options);
    If resut = 1 then ....
    If result = 2 then

---
+++ function.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import cs1.Keyboard;
+public class function
+{
+	public static int show_message (String mess, String[] options)
+	{
+		System.out.println(mess);
+		for ( String s : options)
+		{	int index = Arrays.asList(options).indexOf(s)+1;
+			System.out.println(index + ":" +s);
+		}
+		int choice = Keyboard.readInt();
+		while (0 > choice || choice > options.length)
+			{
+				show_message("invalid choice! please pick again");
+				choice = Keyboard.readInt();	
+			}
+		return choice;
+	}
+	public static void show_message(String mess)
+	{System.out.println(mess);}
+	
+}
