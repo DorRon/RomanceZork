8b795ff7b6db1a5a02cbcb2c173159b7061c434f
renzhentaxi <renzhentaxibaerde@gmail.com>
2014-03-23 04:16:57 -0400

    Update scene2.java

--- scene2.java
+++ scene2.java
@@ -1,18 +1,28 @@
 import java.util.Scanner;
-class scene2
+public class scene2
 {
 	private int[] _scores =  new int[4];
-	private int dumbScore,smartScore,quietScore,freakScore;
+	private String _name;
+	private int dumbScore,smartScore,quietScore,freakScore,hScore;
 	public void scene(String name, int[] scores)
 	{
-		_scores = scores;
+		switch (hScore)
+		{
+			case 0 : System.out.print("dumb"); break;
+			case 1 : System.out.print("smart"); break;
+			case 2 : System.out.print("freak"); break;
+			case 3 : System.out.print("quiet"); break;
+			
+		}
+		
 	}	
-	scene2( int[] scores)
+	public scene2(String name, int[] scores)
 	{
+		_scores =scores;
+		_name = name;
 		dumbScore = scores[0];
 		smartScore = scores[1];
 		quietScore = scores[2];
 		freakScore = scores [3];
-		
 	}
 }
