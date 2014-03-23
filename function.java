63c2dd0ef8f786af3e8c908e58c040f31d80e01b
renzhentaxi <renzhentaxibaerde@gmail.com>
2014-03-23 08:19:05 -0400

    Update function.java

--- function.java
+++ function.java
@@ -1,7 +1,6 @@
 import java.util.*;
-import cs1.Keyboard;
 public class function
-{
+{	public static Scanner sc = new Scanner(System.in);
 	public static int show_message (String mess, String[] options)
 	{
 		System.out.println(mess);
@@ -9,19 +8,18 @@ public class function
 		{	int index = Arrays.asList(options).indexOf(s)+1;
 			System.out.println(index + ":" +s);
 		}
-		int choice = Keyboard.readInt();
+		int choice = sc.nextInt();
 		while (0 > choice || choice > options.length)
 			{
 				show_message("invalid choice! please pick again");
-				choice = Keyboard.readInt();	
+				choice = sc.nextInt();	
 			}
 		return choice;
 	}
+	public static void show_message(String mess, String speaker)
+	{System.out.println(speaker+":\""+mess+"\"");}
+
 	public static void show_message(String mess)
 	{System.out.println(mess);}
-       
-        public static void show_message(String mess, String speaker)
-	{System.out.println(speaker+":"+mess);}
-	
+	public static void nl() {System.out.println();}
 }
-
