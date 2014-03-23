450e03c354bdbe670a1fce3139891f160faf7a38
Zabari <zabari.daniel@Gmail.com>
2014-03-23 04:11:06 +0000

    Intro

--- Intro.java
+++ Intro.java
@@ -3,22 +3,11 @@
                                               Romantic Accord 
 */
 import java.util.Scanner;
-public class RZ{    
+public class Intro{    
     private int player_score = 15; //init player score in game 
     private String user_name;
     private Scanner sc= new Scanner (System.in);
-    public RZ(){}
-    public String getName(){
-       return user_name;
-    }
-    public String setName(String s){
-        String tmp=user_name;
-        user_name=s;
-        return tmp;
-    }
-    public static void main(String[] args){
-        RZ game=new RZ();
-        String welcomeMessage = "A romance RPG aiming to assist those in possibly upcoming stressful situations.\n"
+    private String final WELCOME = "A romance RPG aiming to assist those in possibly upcoming stressful situations.\n"
                          +  "This RPG follows the Zork format, and is solely text-based.\n"
                          +  "You start by answering a questionaire in the first scene,\n"
                          +  "Your decisions will impact the reactions of the partner you choose to pursue throughout the game,\n"
@@ -28,12 +17,21 @@ public class RZ{
                          +  "But it could also make her more eager to get to know you.\n"
                          +  "Make your own calculations and hope for the best.\n"
                          +  "Make the right choices and enjoy the game.";
-        System.out.println("\t\t\t***Romantic Accord***\n");
+    public Intro(){}
+    public String getName(){
+       return user_name;
+    }
+    public String setName(String s){
+        String tmp=user_name;
+        user_name=s;
+        return tmp;
+    }
+    public void start (){
+        System.out.println("\t\t\t***A Jaunt Through Romance***\n");
         System.out.println(welcomeMessage);
         System.out.print("Enter your first name here: ");
         game.setName(sc.next());
         System.out.println("Welcome to the Romantic Accord " + user_name + "\n\n");
-        scene1.main();
     }
 
     
