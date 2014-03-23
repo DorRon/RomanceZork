5ac430abef4e42a3dd46abb4b427d73cf338d152
Zabari <zabari.daniel@Gmail.com>
2014-03-23 09:45:21 +0000

    new format

--- Driver.java
+++ Driver.java
@@ -1,19 +1,30 @@
 import cs1.Keyboard;
 
 public class Driver{
-	private int _dumb;
-	private int _smart;
-	private int _quiet;
-	private int _emo;
+	private String _her;
+	private int _score;
 	private int[] _scores=new int[4];
 	private String _name; 
+	private String[] _names={"Ashley","Patty","Julie","Gabbi"};
+	private String iso(int[] scores){
+		int tmp=-1000000;
+		String tmp2;
+		for (int i=0;i<scores.length;i++){
+			if (scores[i]>tmp){
+				tmp=scores[i];
+				tmp2=_names[i];
+			}
+		}
+		return tmp2;
+	}
 	public void run(){
 		Intro begin=new Intro();
 		_name =begin.start();
 		scene1 scene=new scene1();
 		_scores=scene.scene(_name);
-		scene2 nscene=new scene2(_name,_scores);
-		_scores=nscene.scene();
+		_her=iso(_scores);
+		scene2 nscene=new scene2(_name,_score);
+		_score=nscene.scene();
 	}
 	public static void main(String [] args){
 		Driver game=new Driver();
--- Scene3.java
+++ Scene3.java
@@ -6,27 +6,12 @@ public class Scene3{
 	private int _quiet;
 	private int _emo;
 	private String _topGirl;
-	private String[] _names={"Ashley","Patty","Julie","Gabbi"};
 	public Scene3(String name,int[] scores){
 		_scores=scores;
 		_name=name;
 		_topGirl=iso(scores);
 	}
-	private String iso(int[] scores){
-		int tmp=-1000000;
-		String tmp2;
-		_dumb=scores[0];
-		_smart=scores[1];
-		_quiet=scores[2];
-		_emo=scores[3];
-		for (int i=0;i<scores.length;i++){
-			if (scores[i]>tmp){
-				tmp=scores[i];
-				tmp2=_names[i];
-			}
-		}
-		return tmp2;
-	}
-
+	
+	public int scene(){}
 
 }
