18deaf79ab8278546d91a623155dc7e8f28aa522
DorRon <dorxrondel@gmail.com>
2014-03-22 21:13:19 -0400

    Create scene1.java
    scene 1 for Romantic Accord

---
+++ scene1.java
@@ -0,0 +1,9 @@
+//Scene 1 for Romantic Accord
+public class scene1{
+    public static void main(){
+        System.out.println("So " + RZ.user_name + " you're in an expensive Italian restaurant, on a date with your dream girl.\n");
+        System.out.println("You're eating Fettuccini Alfredo and she's eating Gnocchi.\n");
+        System.out.println("She accidently drops drops her fork, and stains her dress.\n");
+        System.out.println("Do you help her clean her clean up (type help), or do do nothing (type stay)");
+    }
+}
